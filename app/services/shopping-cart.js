aa51e5eb5c5be81ef2911cfcc1445aa83d769989
Alex Jacks and Matt Reyes <alex@>
2016-08-25 11:43:38 -0700

    refactored shopping cart service to display running total in navbar and cart both

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -2,8 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   items: [],
+  totalPrice: 0,
 
   add(game) {
+    this.set('totalPrice', this.get('totalPrice') + parseInt(game.get('price')));
     this.get('items').pushObject(game);
   }
 });
